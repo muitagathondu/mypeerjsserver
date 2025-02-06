527b05ed0d15dc843a9bae7b00ac0fb2ca7e4b62
muitagathondu <91227327+muitagathondu@users.noreply.github.com>
2025-02-06 06:31:26 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { PeerServer } = require('peer');
 const peerServer = PeerServer({
-    port: process.env.PORT || 9000,
+    port: process.env.PORT || 443,
     path: '/peerjs',
     allow_discovery: true, // Optional, enables peer discovery
 });
